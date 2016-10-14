2b137fc046b476ea60ed48dd9d59c7c13888b3f7
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-10-14 17:41:32 +1000

    Edit item_test to change completed value from False to 'r' to standardise

--- item_test.py
+++ item_test.py
@@ -2,7 +2,7 @@ from item import Item
 
 
 def main():
-    item1 = Item("Watch", 24.60, 2, False)
+    item1 = Item("Watch", 24.60, 2, 'r')
     print(item1)
     item1.complete_item()
     print(item1.completed)
