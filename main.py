9f701ea338b1496ce714d54d5135b3e49bee5332
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-10-16 14:53:36 +1000

    Edit dynamic coding in main. Create GUI in app.kv, referring to prac examples like isdynamic

--- main.py
+++ main.py
@@ -13,19 +13,60 @@ from item import Item
 from itemlist import ItemList
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 import csv
 
 
-class ShoppingList(App):
+class ShoppingListApp(App):
+    """ Main program - Kivy app to demo dynamic widget creation """
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """
+        Construct main app
+        """
+        super().__init__(**kwargs)
+        self.item_list = load_items()
+
     def build(self):
+        """
+        Build the Kivy GUI
+        :return: reference to the root Kivy widget
+        """
         self.title = "Shopping List 2.0"
         self.root = Builder.load_file('app.kv')
+        # self.create_widgets()
         return self.root
 
-        items_as_lists = load_items()
-        items_as_lists
+    # def create_widgets(self):
+        """
+        Create buttons from dictionary entries and add them to the GUI
+        :return: None
+        """
+        # for item in self.item_list:
+            # create a button for each phonebook entry
+            # temp_button = Button(text=item)
+            # temp_button.bind(on_release=self.press_entry)
+            # add the button to the "entriesBox" using add_widget()
+            # self.root.ids.entriesBox.add_widget(temp_button)
+
+    # def press_entry(self, instance):
+        """
+        Handler for pressing entry buttons
+        :param instance: the Kivy button instance
+        :return: None
+        """
+        # update status text
+        # item = instance.text
+        # self.status_text = "{}'s number is {}".format(item, self.item_list[item])
+
+    # def clear_all(self):
+        """
+        Clear all of the widgets that are children of the "entriesBox" layout widget
+        :return:
+        """
+        # self.root.ids.entriesBox.clear_widgets()
 
 
-# create and start the App running
-ShoppingList().run()
+ShoppingListApp().run()
