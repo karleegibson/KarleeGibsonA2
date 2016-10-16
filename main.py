52bba679d02bc0a1581b5f0c29d226ba31e32607
karlee.gibson <karlee.gibson@my.jcu.edu.au>
2016-10-16 15:54:07 +1000

    Add code for create_widgets method. Ensure code creates buttons only for required items, using a condition. Customise background colour of buttons depending on the item's priority.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from item import Item
 from itemlist import ItemList
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
 import csv
@@ -27,7 +28,10 @@ class ShoppingListApp(App):
         Construct main app
         """
         super().__init__(**kwargs)
-        self.item_list = load_items()
+        self.items_as_lists = load_items()
+        self.item_list = ItemList()
+        self.item_list.add_items_from_list(self.items_as_lists)
+        self.item_list.sort_items()
 
     def build(self):
         """
@@ -36,22 +40,29 @@ class ShoppingListApp(App):
         """
         self.title = "Shopping List 2.0"
         self.root = Builder.load_file('app.kv')
-        # self.create_widgets()
+        self.create_widgets()
         return self.root
 
-    # def create_widgets(self):
+    def create_widgets(self):
         """
         Create buttons from dictionary entries and add them to the GUI
         :return: None
         """
-        # for item in self.item_list:
-            # create a button for each phonebook entry
-            # temp_button = Button(text=item)
-            # temp_button.bind(on_release=self.press_entry)
+        for item in self.item_list.items:
+            if item.completed == 'r':
+                # create a button for each item entry
+                temp_button = Button(text=item.name)
+                temp_button.bind(on_release=self.press_entry)
+                if item.priority == 1:
+                    temp_button.background_color = (255, 0, 0, 0.7)
+                elif item.priority == 2:
+                    temp_button.background_color = (0, 0, 255, 0.7)
+                else:
+                    temp_button.background_color = (0, 128, 0, 0.7)
             # add the button to the "entriesBox" using add_widget()
-            # self.root.ids.entriesBox.add_widget(temp_button)
+                self.root.ids.entriesBox.add_widget(temp_button)
 
-    # def press_entry(self, instance):
+    def press_entry(self, instance):
         """
         Handler for pressing entry buttons
         :param instance: the Kivy button instance
@@ -61,12 +72,13 @@ class ShoppingListApp(App):
         # item = instance.text
         # self.status_text = "{}'s number is {}".format(item, self.item_list[item])
 
-    # def clear_all(self):
+        # def clear_all(self):
         """
         Clear all of the widgets that are children of the "entriesBox" layout widget
         :return:
         """
         # self.root.ids.entriesBox.clear_widgets()
+        pass
 
 
 ShoppingListApp().run()
