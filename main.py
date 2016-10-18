72383de42ff184d635360899d1b219c4c005bb4d
karleegibson <karlee.gibson@my.jcu.edu.au>
2016-10-18 19:36:06 +1000

    Edit code for complete an item method in main so that item disappears when clicked on. Create a display completed items method so when the user clicks list completed, completed items appear

--- main.py
+++ main.py
@@ -64,6 +64,23 @@ class ShoppingListApp(App):
                     # add the button to the "entriesBox" using add_widget()
                 self.root.ids.entriesBox.add_widget(temp_button)
 
+    def display_completed_items(self):
+        self.root.ids.entriesBox.clear_widgets()
+        self.root.ids.priceOrCompletedLabel.text = 'Showing completed items'
+
+        for item in self.item_list.items:
+            if item.completed == 'c':
+                # create a button for each item entry
+                temp_button = Button(text=item.name)
+                # temp_button.bind(on_release=self.complete_an_item)
+                # add the button to the "entriesBox" using add_widget()
+                self.root.ids.entriesBox.add_widget(temp_button)
+
+        """
+        Clear all of the widgets that are children of the "entriesBox" layout widget
+        :return:
+        """
+
     def complete_an_item(self, instance):
         """
         Handler for pressing entry buttons
@@ -72,21 +89,12 @@ class ShoppingListApp(App):
         """
         # update status text
         item = instance.text
+        self.root.ids.entriesBox.remove_widget(instance)
         item = self.item_list.get_item_by_name(item)
         item = Item(item.name, item.price, item.priority, item.completed)
         item.complete_item()
+        self.root.ids.priceOrCompletedLabel.text = self.item_list.get_total_price()
         self.root.ids.statusLabel.text = 'Completed: {}'.format(item)
-
-    def display_completed_items(self):
-        pass
-
-    def clear_all(self):
-        """
-        Clear all of the widgets that are children of the "entriesBox" layout widget
-        :return:
-        """
-        # self.root.ids.entriesBox.clear_widgets()
-        pass
 
 
 ShoppingListApp().run()
